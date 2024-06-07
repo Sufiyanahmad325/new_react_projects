ea986186c18b80dcf616911150a4fd7770e71d24
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2024-06-07 11:39:04 +0530

    fix some error

--- 20NewQuiz/src/App.jsx
+++ 20NewQuiz/src/App.jsx
@@ -4,7 +4,7 @@ import Button from './Button'
 function App() {
   const [quiz, setQuiz] = useState([
     {
-      question: "What is the capital of France?",
+      question: "Where is the capital of France?",
       answers: [
         { text: "Berlin", correct: false },
         { text: "Madrid", correct: false },
@@ -22,7 +22,7 @@ function App() {
       ]
     },
     {
-      question: "What is the largest ocean on Earth?",
+      question: "Who is the largest ocean on Earth?",
       answers: [
         { text: "Atlantic Ocean", correct: false },
         { text: "Indian Ocean", correct: false },
@@ -36,20 +36,21 @@ function App() {
   const [score, setScore] = useState(0)
 
   const next = () => {
+    if(!selectedValue) return
     check()
   }
 
   const check = () => {
     console.log(selectedValue)
-        if (selectedValue.correct) {
-           setScore(prev => prev + 1)
-        }
+    if (selectedValue.correct) {
+      setScore(prev => prev + 1)
+    }
 
     setCurrentQuestion(prev => prev + 1)
 
   }
 
-  
+
   if (currentQuestion >= quiz.length) {
     return (
       <div className='flex items-center justify-center h-[100vh]'>
@@ -62,21 +63,21 @@ function App() {
   }
   return (
     <>
-    <div className='flex items-center justify-center h-[100vh]'>
-      <div className='w-[600px] bg-gray-200 h-[200px] flex flex-col items-center justify-center gap-6 p-3'>
-        <div className='text-2xl font-bold'>{quiz[currentQuestion].question}</div>
-        <div className='flex gap-2'>
-          {
-            quiz[currentQuestion].answers.map((Element, index) => {
-              return <Button option={Element.text} key={index} data={Element} setSelectedValue={setSelectedValue} />
-            })
-          }
-        </div>
-        <button className='text-1xl py-1 px-5 border-2 border-solid border-blue-800 
+      <div className='flex items-center justify-center h-[100vh]'>
+        <div className='w-[600px] bg-gray-200 h-[200px] flex flex-col items-center justify-center gap-6 p-3'>
+          <div className='text-2xl font-bold'>{quiz[currentQuestion].question}</div>
+          <div className='flex gap-2'>
+            {
+              quiz[currentQuestion].answers.map((Element, index) => {
+                return <Button option={Element.text} key={index} data={Element} setSelectedValue={setSelectedValue} />
+              })
+            }
+          </div>
+          <button className='text-1xl py-1 px-5 border-2 border-solid border-blue-800 
         rounded-md hover:bg-blue-500
         ' onClick={next}>Next</button>
+        </div>
       </div>
-    </div>
     </>
   )
 }
